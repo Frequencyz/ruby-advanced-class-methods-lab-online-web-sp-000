a4033e2d861471ca1ebae29cbebeba88df4b76e2
Andrew Bay <daddnbay@yahoo.com>
2019-07-15 19:52:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,12 @@ class Song
     self.all.sort_by { |song| song.name }
   end 
   
-  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
   
     
     
